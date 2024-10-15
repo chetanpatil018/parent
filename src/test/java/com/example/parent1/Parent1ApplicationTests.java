56d71355aa467009626f1df77db2fb3df82f1733
chetan patil <chetan.patil018@gmail.com>
2024-10-16 00:09:01 +0530

    initial commit

---
+++ src/test/java/com/example/parent1/Parent1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parent1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parent1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
